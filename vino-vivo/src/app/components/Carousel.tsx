e7644f366ee28a2b5db56f93f8d7eff447dbbd34
julioborn <juliobornes10@gmail.com>
2024-05-10 00:30:01 -0300

    Estructura del proyecto

---
+++ vino-vivo/src/app/components/Carousel.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+
+interface CarouselProps {
+    images: string[];
+}
+
+const Carousel: React.FC<CarouselProps> = ({ images }) => {
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 7000); // Cambiar cada 3 segundos
+
+        return () => clearInterval(interval);
+    }, [images.length]);
+
+    return (
+        <div className="relative w-full h-60 rounded-lg border overflow-hidden max-w-xl flex items-center justify-center">
+            {images.map((image, index) => (
+                <img
+                    key={index}
+                    src={image}
+                    alt={`Slide ${index}`}
+                    className={`absolute inset-0 object-fit transition-opacity duration-1000 ${index === currentImageIndex ? 'opacity-100' : 'opacity-0'
+                        }`}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default Carousel;
---
+++ vino-vivo/src/app/components/layouts/footer/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="bg-white p-4 text-center text-violeta w-full h-36 border-t-4 border-red-950">
+
+        </footer>
+    );
+};
+
+export default Footer;
