4c097cb422e41eb4e630d9d03c5c29268b7f15ce
Ivanakowalczuk <ivanakowalczuk@gmail.com>
2024-05-13 02:20:47 -0300

    cambio importaciones por el cambio de carpeta

--- vino-vivo/src/app/detail/page.tsx
+++ vino-vivo/src/app/detail/page.tsx
@@ -1,6 +1,7 @@
-import Footer from "../components/layouts/footer/Footer";
-import Header from "../components/layouts/header/Header";
-import CardDetail from "../components/productDetail/CardDetail";
+import Footer from "@/components/layouts/footer/Footer";
+import Header from "@/components/layouts/header/Header";
+import CardDetail from "@/components/productDetail/CardDetail";
+
 
 export default function detailPage() {
     return(
