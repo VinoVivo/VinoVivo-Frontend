69894a50836d91fd7c957a615f26ee421d500239
Ivanakowalczuk <ivanakowalczuk@gmail.com>
2024-05-22 18:01:35 -0300

    inicio adminMisProductos

---
+++ vino-vivo/src/components/adminMisProductos/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CardDetail from '../product/detail/CardDetail';
+
+const AdminMisProductos = () => {
+  return (
+    <div>
+      
+      <CardDetail/>
+      
+      </div>
+  )
+}
+
+export default AdminMisProductos;
