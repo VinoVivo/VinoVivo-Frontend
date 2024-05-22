51d429a3c0ae0e4ae09998e7ab25d7cf46d00a02
Ivanakowalczuk <ivanakowalczuk@gmail.com>
2024-05-22 17:02:41 -0300

    cambios en title de concepto

--- vino-vivo/src/components/concept/Concept.tsx
+++ vino-vivo/src/components/concept/Concept.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import { Button } from '../ui/button'
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '../ui/carousel'
+import { Title } from '../Title/Title'
 
 
 
@@ -9,17 +10,13 @@ const Concept = () => {
   return (
     <div className='flex-col space-between  w-full px-5 sm:px-2'>
       <section className=' md:px-16  w-11/12 mt-60 sm:px-1 mx-auto '>
-        <div className='flex w-[300px] mx-auto p-4 sm:w-[600px] sm:p-none'>
-            <div className="border-b-4 border-b-beige w-[341px] m-0 hidden  sm:flex" /> 
-            <h1 className='text-xl text-beige w-full text-center m-0'>NUESTRA HISTORIA</h1>
-            <div className="border-b-4 border-b-beige w-[341px] hidden sm:flex" /> 
-        </div>
+        <Title title='Nuestra Historia' color='beige'/>
         <div className='flex flex-col justify-center gap-4 items-center mt-10  sm:flex-row max-w-screen'>
           <div className='flex flex-col justify-between items-center gap-4  md:flex-row  '>
         <Image src='/image 37.png' alt='imagen de cata' width={230} height={400} className=' w-[280px] h-[350px] rounded-md '/>
         
         <div className='min-w-60 max-w-[320px] text-justify flex flex-wrap'>
-        <p className='text-secondary text-sm sm:py-10 md:py-0'>Renato Pagliano, creador de VinoVivo, tiene 38 años.  Casado y con un hijo, vivió en Almagro toda su vida, dedicándose al rubro desde pequeño por por herencia familiar. 
+        <p className='text-secondary text-sm sm:py-10 sm:text-md md:py-0 '>Renato Pagliano, creador de VinoVivo, tiene 38 años.  Casado y con un hijo, vivió en Almagro toda su vida, dedicándose al rubro desde pequeño por por herencia familiar. 
         Pasó casi toda su vida entre cocina y barra, interactuando con las personas y sus distintos paladares. Hace 5 años decidió estudiar enología para ampliar sus conocimientos vitivinícolas, y descubrió que ésta era su pasión. 
         Hoy tiene su propio emprendimiento gastronómico Vino Vivo donde comercializa los mejores vinos y
         donde realiza catas y maridajes personalizados.
@@ -34,11 +31,7 @@ const Concept = () => {
 
       </section>
       <section className=' flex flex-col flex-wrap md:px-24 py-20 w-full sm:px-1 md:flex-row '>
-      <div className='flex w-[300px] mx-auto p-4 sm:w-[600px] sm:p-none'>
-            <div className="border-b-4 border-b-beige w-[341px] m-0 sm:hidden md:flex" /> 
-            <h1 className='text-xl text-beige w-full text-center m-0'>LAS CATAS</h1>
-            <div className="border-b-4 border-b-beige w-[341px] sm:hidden md:flex" /> 
-        </div>
+      <Title title='Las Catas' color='beige'/>
           <Carousel className='flex w-11/12 my-10 m-auto gap-4 py-10'>
             <CarouselContent>
               <CarouselItem className="basis-1/1 md:basis-1/4 sm:basis-1/2">
