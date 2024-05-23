e5882442974a02ac64600d1da279c45e066b7587
Sol <solzito.1889@gmail.com>
2024-05-23 15:35:18 -0300

    se agregan componentes/funciones para validar lo ingresado por el usuario

--- vino-vivo/src/components/contact/form/Data.ts
+++ vino-vivo/src/components/contact/form/Data.ts
@@ -1,6 +1,23 @@
-export interface IFormContact {
-  name: string;
-  number: string;
-  email: string;
-  message: string;
+import { z } from "zod";
+
+export const FormSchema = z.object({
+  username: z.string().min(2, {
+    message: "Nombre debe tener al menos 2 caracteres.",
+  }),
+  usercellphone: z
+    .string({ message: "Ingrese un numero valido" })
+    .regex(/^([0-9])+$/, { message: "Ingrese un numero valido" })
+    .min(10, {
+      message: "Celular debe tener al menos 10 numeros.",
+    }),
+  useremail: z.string().email({
+    message: "Ingrese un correo valido",
+  }),
+  message: z.string(),
+});
+
+export function onSubmit(values: z.infer<typeof FormSchema>) {
+  // Do something with the form values.
+  // ✅ This will be type-safe and validated.
+  alert("Ya nos pondremos en contacto");
 }
