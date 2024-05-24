4eb617b430192a4f306935c2b2b20b189f4afd0d
Sol <solzito.1889@gmail.com>
2024-05-23 21:03:09 -0300

    se agregan validaciones para el formulario

--- vino-vivo/src/components/contact/form/Data.ts
+++ vino-vivo/src/components/contact/form/Data.ts
@@ -1,18 +1,33 @@
 import { z } from "zod";
-
+export type FormValues = {
+  username: string;
+  usercellphone: string;
+  useremail: string;
+  message: string;
+};
 export const FormSchema = z.object({
-  username: z.string().min(2, {
-    message: "Nombre debe tener al menos 2 caracteres.",
-  }),
+  username: z
+    .string()
+    .min(3, {
+      message: "Nombre debe tener al menos 3 caracteres.",
+    })
+    .regex(/(^[A-Za-z]{1,10})([ ]{0,1})([A-Za-z]{2,16})/, {
+      message: "Ingrese un nombre valido",
+    }),
   usercellphone: z
     .string({ message: "Ingrese un numero valido" })
     .regex(/^([0-9])+$/, { message: "Ingrese un numero valido" })
     .min(10, {
       message: "Celular debe tener al menos 10 numeros.",
     }),
-  useremail: z.string().email({
-    message: "Ingrese un correo valido",
-  }),
+  useremail: z
+    .string()
+    .email({
+      message: "Ingrese un correo valido",
+    })
+    .regex(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, {
+      message: "Ingrese un correo valido.",
+    }),
   message: z.string(),
 });
 
