cbf42bedab25df0ab5becc711640f84df17d31a3
Ivanakowalczuk <ivanakowalczuk@gmail.com>
2024-05-25 20:56:54 -0300

    cambios en context

--- vino-vivo/src/context/page.tsx
+++ vino-vivo/src/context/page.tsx
@@ -1,30 +1,39 @@
-import { createContext, useContext, useReducer } from "react";
-
+import { ReactNode, createContext, useContext, useReducer } from "react";
 
 const ContextGlobal = createContext({});
 
-const initialProductState = {productList:[], productDetail:{}};
+const initialProductState = { productList: [], productDetail: {} };
 
-const productReducer = (state: any, action:any) => {
-  switch(action.type){
-   case 'GET_PRDDUCTS':
-        return {productList: action.payload, productDetail: state.productDetail}
-    case 'GET_PRDDUCT':
-        return {productDetail: action.payload, productList: state.productDetail}
-
+const productReducer = (state:any, action:any) => {
+  switch (action.type) {
+    case 'GET_PRODUCTS':
+      return { productList: action.payload, productDetail: state.productDetail };
+    case 'GET_PRODUCT':
+      return { productDetail: action.payload, productList: state.productList };
+    default:
+      return state;
   }
-
-}
-
-const ContextProvider = ({children}:{children:any}) =>{
-    const [productListState, productListDispatch] = useReducer(productReducer, initialProductState)
-    return(
-      
-        <ContextGlobal.Provider value={{productListState, productListDispatch}}>
-         {children}
-        </ContextGlobal.Provider>
-    );
 };
 
-export default ContextProvider
-export const useGlobalStates=() => useContext(ContextGlobal)
+const ContextProvider = ({ children}:{children:ReactNode}) => {
+  const [productListState, productListDispatch] = useReducer(productReducer, initialProductState);
+
+  const baseUrl: string = 'http://localhost:8082';
+
+  const getProductList = () => {
+    const url: string = `${baseUrl}/product/type/all`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => productListDispatch({ type: 'GET_PRODUCTS', payload: data }))
+      .catch(error => console.error('Error fetching products:', error));
+  };
+
+  return (
+    <ContextGlobal.Provider value={{ productListState, productListDispatch, getProductList }}>
+      {children}
+    </ContextGlobal.Provider>
+  );
+};
+
+export default ContextProvider;
+export const useGlobalStates = () => useContext(ContextGlobal);
