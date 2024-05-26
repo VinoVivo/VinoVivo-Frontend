3c5370fcad5d1847c832189537b044ed282ed3ac
Ivanakowalczuk <ivanakowalczuk@gmail.com>
2024-05-26 01:34:55 -0300

    card mis productos

--- vino-vivo/src/components/adminMisProductos/cardProduct.tsx
+++ vino-vivo/src/components/adminMisProductos/cardProduct.tsx
@@ -2,32 +2,37 @@ import React from 'react'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '../ui/card';
 import { Button } from '../ui/button';
 import Image from 'next/image';
-
-
-
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const CardProduct = ({product}:{product:Product}) => {
+   
   return (
-   <Card>
-    <CardHeader>
-        <CardTitle>
-            {product.name}
-        </CardTitle>
-       
-    </CardHeader>
+   <Card className=' bg-background  rounded-lg border border-gray-200 p-6 w-full sm:w-64'>
+    
     <CardContent>
-        
+    <Link href={`/product/register`}>
     <Image
     src={product.image} 
-    width={500}
-      height={500}
-      alt="Picture of the author"></Image> 
-    {product.description}
+    width={200}
+      height={200}
+      alt="Picture of the author"
+      className="w-full h-auto transform transition-transform duration-300 hover:scale-105"
+      ></Image>             
+    </Link>
+     <div className="flex flex-col items-center mt-2">
+       <p className="text-md font-bold text-black h-12 text-center">{product.name}</p>
+       <p className="text-sm text-black mt-2">{product.idVariety}</p>
+       <p className="text-md font-semibold text-black">${product.price}</p>
+     </div>
+   
     </CardContent>
     <CardFooter>
-        <Button>
-           Editar
+    <Link href="/product/register" className='w-full'>
+        <Button className='hover:bg-violeta font-bold mt-2 py-1.5 px-4 rounded w-full'>
+            Editar
         </Button>
+    </Link>
     </CardFooter>
 
    </Card>
--- vino-vivo/src/components/adminMisProductos/page.tsx
+++ vino-vivo/src/components/adminMisProductos/page.tsx
@@ -15,7 +15,7 @@ const ProductList = () => {
   }, [ productListState ]);
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-[150px]">
       {productListState?.productList.map((product: Product,  index: number) => (
         <CardProduct key={index} product={product} />
       ))}
