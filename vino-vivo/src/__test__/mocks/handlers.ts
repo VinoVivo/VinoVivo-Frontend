b19f08e616f48337708ae4a5f74a41db5ef5c2b7
jeremoak <jereak@hotmail.com>
2024-05-28 14:36:41 -0300

    arreglando problemas config

--- vino-vivo/src/__test__/mocks/handlers.ts
+++ vino-vivo/src/__test__/mocks/handlers.ts
@@ -1,44 +1,39 @@
-import { setupServer } from 'msw/node';
-import { http, RequestHandler } from 'msw';
 
-interface CustomHttp {
-  [key: string]: RequestHandler;
-}
+import { http, HttpResponse } from 'msw';
+import { server } from './server';
+
+server.listen()
 
 const handlers = [
-  {
-    method: 'get', 
-    url: 'http://localhost:8082/product/all',
-    response: (req: any, res: any, ctx: any) => {
-      return res(
-        ctx.status(200),
-        ctx.json([
-          { id: 1, name: 'Product 1', description: 'Description 1', image: '/image1.jpg', price: 100, idVariety: '1' },
-          { id: 2, name: 'Product 2', description: 'Description 2', image: '/image2.jpg', price: 200, idVariety: '2' }
-        ])
-      );
-    }
-  },
-  {
-    method: 'get', 
-    url: 'http://localhost:8082/variety/id/1',
-    response: (req: any, res: any, ctx: any) => {
-      return res(
-        ctx.status(200),
-        ctx.json({ name: 'Variety 1' })
-      );
-    }
-  },
-  {
-    method: 'get', 
-    url: 'http://localhost:8082/variety/id/2',
-    response: (req: any, res: any, ctx: any) => {
-      return res(
-        ctx.status(200),
-        ctx.json({ name: 'Variety 2' })
-      );
-    }
-  }
+
+
+  http.get('http://localhost:8082/product/all', () => {
+  
+    return HttpResponse.json(
+      [
+        { id: 1, name: 'Product 1', description: 'Description 1', image: '/image1.jpg', price: 100, idVariety: '1' },
+        { id: 2, name: 'Product 2', description: 'Description 2', image: '/image2.jpg', price: 200, idVariety: '2' }
+      ])
+  }),
+
+  http.get('http://lhttp://localhost:8082/variety/id/1ocalhost:8082/product/id/1', () => {
+  
+  return HttpResponse.json(
+    [
+      { name: 'Variety 1' }
+    ])
+}),
+
+http.get('http://localhost:8082/variety/id/2', () => {
+  
+return HttpResponse.json(
+  [
+    { name: 'Variety 2' }
+  ])
+}),
+
+
+
 ];
 
 
