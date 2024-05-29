fdf714a3a6e755bda029945b862b851b031b347a
Ivanakowalczuk <ivanakowalczuk@gmail.com>
2024-05-29 19:25:44 -0300

    cambios

--- vino-vivo/src/app/product/update/[id]/page.tsx
+++ vino-vivo/src/app/product/update/[id]/page.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import UpdateProductForm from '@/components/product/update/page';
+
 import { IwineDetail } from '@/types/detail/detail.types';
 import { notFound } from 'next/navigation';
 import { Product, ProductFormValues } from '@/types/products/products.types';
+import UpdateProductForm from '@/components/product/update/page';
 
 
 
 
 interface DetailPageProps {
     params: {
-id: number| undefined;
+id: string | undefined;
     };
 }
 
-async function fetchProductData(id: number | undefined): Promise<Product | null> {
+async function fetchProductData(id: string | undefined): Promise<Product | null> {
     try {
     const response = await fetch(`http://localhost:8082/product/id/${id}`, {
       cache: 'no-store', // Esto asegura que se obtengan datos frescos en cada solicitud.
@@ -26,7 +27,7 @@ async function fetchProductData(id: number | undefined): Promise<Product | null>
     const productData = await response.json();
 
     const product: Product = {
-        id: Number(productData.id),
+        id: productData.id,
         name: productData.name,
         description: productData.description,
         image: productData.image,
@@ -45,7 +46,7 @@ async function fetchProductData(id: number | undefined): Promise<Product | null>
 }
 
 export default async function UpdatePageId({ params }: Readonly<DetailPageProps>) {
-    const  product: Product | null = await fetchProductData(params.id);
+    const  product = await fetchProductData(params.id);
 
     if (!product) {
         notFound;
