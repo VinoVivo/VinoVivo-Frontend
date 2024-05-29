3108d37dce36ab1d6aa87d9ffc0828265e8f6ec9
Sol <solzito.1889@gmail.com>
2024-05-29 10:51:56 -0300

    se modifica para exportar el product e importarlo en otros componentes

--- vino-vivo/src/types/products/products.types.ts
+++ vino-vivo/src/types/products/products.types.ts
@@ -1,22 +1,22 @@
-interface Product {
-    id: number;
-    name: string;
-    description: string;
-    image: string;
-    price: number;
-    nameWinery: string,
-    nameVariety: string,
-    nameType: string
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+  price: number;
+  nameWinery: string;
+  nameVariety: string;
+  nameType: string;
 }
 
 export interface ProductFormValues {
-    name: string;
-    description: string;    
-    image: string;
-    year: number;
-    price: number; 
-    stock: number;   
-    idWinery: number;
-    idVariety: number;    
-    idType: number;                   
-};
+  name: string;
+  description: string;
+  image: string;
+  year: number;
+  price: number;
+  stock: number;
+  idWinery: number;
+  idVariety: number;
+  idType: number;
+}
