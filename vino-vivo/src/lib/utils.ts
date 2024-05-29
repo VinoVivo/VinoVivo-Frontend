fd8fca7981f983eff7c0e6780e3d91b556cd736c
Sol <solzito.1889@gmail.com>
2024-05-29 10:53:15 -0300

    se importa correctamente product, modificaciones con prettier

--- vino-vivo/src/lib/utils.ts
+++ vino-vivo/src/lib/utils.ts
@@ -1,41 +1,39 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { Product } from "@/types/products/products.types";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-
-const baseUrl: string = 'http://localhost:8082';
+const baseUrl: string = "http://localhost:8082";
 
 export const getProductList = async (): Promise<Product[]> => {
   const url: string = `${baseUrl}/product/type/all`;
 
   try {
     const response = await fetch(url);
-    
+
     if (!response.ok) {
-      throw new Error('Error fetching products'); // Lanzar un error si la solicitud no es exitosa
+      throw new Error("Error fetching products"); // Lanzar un error si la solicitud no es exitosa
     }
-    
+
     const data: Product[] = await response.json();
-    
-    console.log('dataPagination', data);
-    
+
+    console.log("dataPagination", data);
+
     return data;
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
     throw error; // Lanzar el error para que pueda ser manejado por el código que llama a esta función
   }
 };
 
-export const getProduct = async (url:string) => {
-
+export const getProduct = async (url: string) => {
   const response = await fetch(url);
   if (!response.ok) {
-    throw new Error('Error fetching products');
+    throw new Error("Error fetching products");
   }
   const products = await response.json();
   return products;
 };
-
