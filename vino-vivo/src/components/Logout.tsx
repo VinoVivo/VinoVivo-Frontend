4d7341eecf3109b27e200f655628f9f8a43262fb
julioborn <juliobornes10@gmail.com>
2024-06-05 21:12:50 -0300

    Solución de logout de keycloak y next-auth

--- vino-vivo/src/components/Logout.tsx
+++ vino-vivo/src/components/Logout.tsx
@@ -1,11 +1,10 @@
 "use client";
-
-import { signOut } from "next-auth/react";
+import federatedLogout from "@/app/api/auth/federated-logout/utils";
 
 export default function Logout() {
   return (
     <button
-      onClick={() => signOut()}
+      onClick={() => federatedLogout()}
       className="bg-sky-500 hover:bg-sky-700 px-5 py-2 text-sm leading-5 rounded-full font-semibold text-white"
     >
       Signout of keycloak
