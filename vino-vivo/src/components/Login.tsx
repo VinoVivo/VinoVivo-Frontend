ef3a6adaecacc3f0f0868de88b59ea67b6a11158
Sol <solzito.1889@gmail.com>
2024-06-05 22:36:24 -0300

    cambio estilo btn de login y logout

--- vino-vivo/src/components/Login.tsx
+++ vino-vivo/src/components/Login.tsx
@@ -5,7 +5,7 @@ export default function Login() {
   return (
     <button
       onClick={() => signIn("keycloak")}
-      className="hover:bg-pink-800 px-5 py-2 text-sm leading-5 rounded-full font-semibold text-white bg-pink-400"
+      className="hover:bg-primary px-5 py-2 text-sm leading-5 rounded-full font-semibold text-white bg-violeta"
     >
       Iniciar sesion
     </button>
--- vino-vivo/src/components/Logout.tsx
+++ vino-vivo/src/components/Logout.tsx
@@ -5,9 +5,9 @@ export default function Logout() {
   return (
     <button
       onClick={() => federatedLogout()}
-      className="bg-sky-500 hover:bg-sky-700 px-5 py-2 text-sm leading-5 rounded-full font-semibold text-white"
+      className="hover:bg-primary px-5 py-2 text-sm leading-5 rounded-full font-semibold text-white bg-violeta"
     >
-      Signout of keycloak
+      Cerrar sesion
     </button>
   );
 }
