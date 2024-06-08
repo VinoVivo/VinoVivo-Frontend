d52fabbaa2e8f73dff86021621421df531537c68
Ivanakowalczuk <ivanakowalczuk@gmail.com>
2024-06-08 19:28:26 -0300

    saco products de array de useeffect en adminProductos porque hace demasiadas peticiones

--- vino-vivo/src/components/adminMisProductos/page.tsx
+++ vino-vivo/src/components/adminMisProductos/page.tsx
@@ -38,7 +38,7 @@ const ProductGrid = () => {
       setLoading(false);
     };
     fetchProducts();
-  }, [products, loading]);
+  }, []);
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-[150px]">
