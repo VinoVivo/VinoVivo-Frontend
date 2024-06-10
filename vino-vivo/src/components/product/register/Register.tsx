6b168cfff9f73b1c9c2dd996eb37e9aa18098270
Ivanakowalczuk <ivanakowalczuk@gmail.com>
2024-06-10 16:58:40 -0300

    cambio método registrar poroducto

--- vino-vivo/src/components/product/register/Register.tsx
+++ vino-vivo/src/components/product/register/Register.tsx
@@ -34,49 +34,35 @@ export default function Register() {
         fetchData();
     }, []);
 
-    const onSubmit: SubmitHandler<ProductFormValues> = async (data) => {
+    const createProduct = async (data: object) => {
         try {
-            const payload = { ...data };
-
-            const response = await fetch(`${process.env.NEXT_PUBLIC_GET_BASE_URL}/ms-commerce/product/create`, {
+            const response = await fetch('/api/products/register', {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ id: 0, ...payload })
+                body: JSON.stringify(data),
             });
-
+    
             if (!response.ok) {
                 throw new Error('Error en la respuesta del servidor');
             }
-
-            const responseData = await response.json();
-            console.log('Producto creado:', responseData);
             setDialogType("Éxito");
             setDialogMessage('Su producto ha sido creado exitosamente');
+            const responseData = await response.json();
+            return responseData;
         } catch (error) {
-            console.error('Error al crear el producto:', error);
             setDialogType("Error");
             setDialogMessage('Su producto no ha podido ser creado, por favor revise los datos e intente nuevamente');
-        } finally {
+            console.error('Error al crear el producto:', error);
+            throw error;
+        }finally{
             setDialogOpen(true);
         }
-
-        // para probar unicamente los cuadros de dialogo sin crear un producto: 
-        // const simulateSuccess = true; // Cambia esto a false para probar el caso de error
-        // if (simulateSuccess) {
-        //     console.log('Producto creado:', data);
-        //     setDialogType("success");
-        //     setDialogMessage('Su producto ha sido creado exitosamente');
-            
-        // } else {
-        //     console.error('Error al crear el producto');
-        //     setDialogType("error");
-        //     setDialogMessage('Su producto no ha podido ser creado, por favor revise los datos e intente nuevamente');
-        // }        
-        // setDialogOpen(true);
     };
-    
+
+
+
 
     return (
         <div className="max-w-4xl mx-4 my-40 lg:mx-auto">
@@ -84,7 +70,7 @@ export default function Register() {
                 <Title title="Registro de Producto" color="beige"/>
                 <span className="ml-2 sm:mt-0"><BackButton/></span>
             </div>            
-            <ProductForm onSubmit={onSubmit} wineries={wineries} types={types} varieties={varieties}/>
+            <ProductForm onSubmit={createProduct} wineries={wineries} types={types} varieties={varieties}/>
             <DialogeRegister open={dialogOpen}  onOpenChange={setDialogOpen} type={dialogType} message={dialogMessage} textButtonOne={'cerrar'}/>
         </div>
     );
