09702997b03b20409ada6ba6e7591c01706a6ef7
Liza Medina <lizzmedina15@gmail.com>
2024-06-13 11:13:13 -0500

    paginación de todos los productos

--- vino-vivo/src/components/accordion/page.tsx
+++ vino-vivo/src/components/accordion/page.tsx
@@ -19,8 +19,8 @@ const Accordion: React.FC<AccordionProps> = ({ title, children }) => {
                 className="w-full px-4 py-2 text-left font-semibold bg-gray-100 rounded-t-lg focus:outline-none flex justify-between"
             >
                 {title}
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-4 mt-1">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-4 mt-1">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
                 </svg>
             </button>
             {isOpen && (
