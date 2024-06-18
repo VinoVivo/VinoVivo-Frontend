12ab9c884491b929556b4e14e9ba0daabc211148
Sol <solzito.1889@gmail.com>
2024-06-17 21:52:49 -0300

    user types

---
+++ vino-vivo/src/types/user/userprofile.types.ts
@@ -0,0 +1,15 @@
+export type userData = {
+  id: string;
+  userName: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  dni: string;
+  cellphone: string;
+  state: string;
+  city: string;
+  address: string;
+  photo: string;
+
+  observation?: string;
+};
