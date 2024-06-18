de47c70e2ef06e3b59b1318d01f0bb4d566fce23
Sol <solzito.1889@gmail.com>
2024-06-17 21:53:15 -0300

    se crea tabla para mostrar nfor del user

---
+++ vino-vivo/src/components/data-user/TableAddres.tsx
@@ -0,0 +1,67 @@
+import { userData } from "@/types/user/userprofile.types";
+import React from "react";
+
+export const TableAddress = ({
+  firstName,
+  lastName,
+  address,
+  email,
+  dni,
+  cellphone,
+}: userData) => {
+  return (
+    <div className="border border-gray-200 rounded-md p-4 w-full mt-40 mx-auto">
+      <div className="border-b border-gray-200">
+        <h2 className="text-lg font-bold mb-2"> Domicilio de envio:</h2>
+      </div>
+      <div className="mt-6 border-t border-gray-100">
+        <dl className="divide-y divide-gray-100">
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Nombre:
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {`${firstName} ${lastName}`}
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Direccion:
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {address}
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Email:
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {email}
+            </dd>
+          </div>
+          {dni && (
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                Dni:
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {dni}
+              </dd>
+            </div>
+          )}
+          {cellphone && (
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                Celular:
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {cellphone}
+              </dd>
+            </div>
+          )}
+        </dl>
+      </div>
+    </div>
+  );
+};
