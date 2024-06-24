505e1c80194b034fa23ec89d3200dc05da2787e5
Ivanakowalczuk <ivanakowalczuk@gmail.com>
2024-06-23 22:51:34 -0300

    cambios en footer

--- vino-vivo/src/components/layouts/footer/Footer.tsx
+++ vino-vivo/src/components/layouts/footer/Footer.tsx
@@ -9,8 +9,8 @@ function Footer() {
     return (
         <footer className="bg-card border-t-2 border-primary">
 
-            <div className="container mx-auto">
-                <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-6 py-7 ">
+            
+                <div className="flex flex-col justify-center items-center md:flex-row md:justify-evenly md:py-[8px]">
 
                     <div className="m-2 space-y-2 flex flex-col justify-center items-center sm:col-start-1 sm:col-span-2">
                         <p className="text-secondary text-sm">Vino Vivo @Copyright</p>
@@ -18,20 +18,20 @@ function Footer() {
                         <p className="text-secondary text-sm">Diseño gráfico por CA</p>
                     </div>
 
-                    <div className="m-2 sm:col-span-2  flex flex-col ">
+                    <div className="m-2 flex flex-col ">
                         <div >
                             <h1 className="max-w-lg text-sm font-semibold tracking-tight text-grisCarbon ">
                                 NEWSLETTER
                             </h1>
                         </div>
-                        <div className="flex flex-col border border-grisCarbon  mt-1 space-y-3 md:space-y-0 md:flex-row">
+                        <div className="flex  ">
                             <input id="email"
                                 type="text"
-                                className="px-2 py-1 bg-background border rounded-md text-primary  focus:border-gray-400 dark:focus:border-gray-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-gray-300"
+                                className="px-2 py-1 border border-grisCarbon  bg-background  rounded-md text-primary text-sm focus:border-gray-400 dark:focus:border-gray-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-gray-300"
                                 placeholder="Ingresá aquí tu email"
                             />
 
-                            <Button className='bg-grisCarbon active:bg-gray-250 hover:bg-gris w-full rounded-l-none rounded-r-[1px]'>SUSCRIBETE</Button>
+                            <button className='bg-grisCarbon text-xs text-white px-2 active:bg-gray-250 hover:bg-gris rounded-l-none rounded-r-[1px]'>SUSCRIBETE</button>
                         </div>
 
                         <div className="self-center flex items-center justify-between mt-3">
@@ -54,7 +54,7 @@ function Footer() {
                         </div>
                     </div>
 
-                    <div className="m-2 flex flex-col justify-center items-center space-y-2 h-full sm:col-start-5 sm:col-span-2">
+                    <div className="m-2 flex flex-col justify-center items-center space-y-2 h-full ">
                         <a href="#" className="text-sm transition-colors duration-300 dark:text-gray-300 hover:underline">Políticas de privacidad</a>
                         <a href="#" className="text-sm transition-colors duration-300 dark:text-gray-300 hover:underline">Términos y condiciones</a>
                         <a href="#" className="text-sm transition-colors duration-300 dark:text-gray-300 hover:underline">Prensa</a>
@@ -62,7 +62,7 @@ function Footer() {
 
                 </div>
 
-            </div>
+          
         </footer>
     )
 }
